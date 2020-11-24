d2fb670c010f2cbac7bd5cc734feca1eaf150bb9
HOWARD CHAN (MIS-ISD-OOCLL/HKG) <CHANHO@OOCL.COM>
2020-11-24 17:01:14 +0800

    [10 Multiplication Table][]

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -1,7 +1,55 @@
 package com.tw;
 
 public class MultiplicationTable {
-    public String create(int startNumber, int endNumber) {
-        return null;
+    public static void main(String[] args) {
+        MultiplicationTable test = new MultiplicationTable();
+        System.out.println(test.create(2,4));
+    }
+
+    public String create(int start, int end) {
+        /*
+        Plan:2min
+        Do:15min
+        Check:a little bit messed up in the nested loop
+        Act:practice more nested loop
+        */
+        boolean areValid = areInputsValid(start,end);
+        return areValid ? implementMultiplicationTable(start,end) : null;
+    }
+
+    private String implementMultiplicationTable(int start, int end) {
+        /*
+        Plan:1mingit
+        Do:14min
+        Check:a little bit messed up in the nested loop
+        Act:practice more nested loop
+        */
+        String table="";
+        for(int i=start;i<=end;i++) {
+            for(int j=start;j<=i;j++) {
+                table += j + "*" + i + "=" + i * j;
+                if (j != i) {
+                    table += "  ";
+                }
+            }
+            if(i!=end)
+            {
+                table+=System.lineSeparator();
+            }
+        }
+        return table;
+    }
+
+    private boolean areInputsValid(int start, int end) {
+        /*
+        Plan:1min
+        Do:1min
+        Check:all good
+        Act:no need
+        */
+        if(start>end || start<1 || end>1000) {
+            return false;
+        }
+        return true;
     }
 }
